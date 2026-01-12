20e68cc6b7f6eab3bd46089e880af88c22ba1afd
alex.e <a.ermakov>
2026-01-12 23:37:14 +0200

    fixed sql

--- db_dump/vacation.sql
+++ db_dump/vacation.sql
@@ -1,50 +1,21 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1:3306
--- Время создания: Янв 12 2026 г., 23:23
--- Версия сервера: 8.0.30
--- Версия PHP: 8.1.9
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- База данных: `vacation`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `roles`
---
 
 CREATE TABLE `roles` (
   `id` int NOT NULL,
   `name` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Дамп данных таблицы `roles`
---
-
 INSERT INTO `roles` (`id`, `name`) VALUES
 (1, 'Requester'),
 (2, 'Validator');
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `users`
---
 
 CREATE TABLE `users` (
   `id` int NOT NULL,
@@ -52,20 +23,10 @@ CREATE TABLE `users` (
   `role_id` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Дамп данных таблицы `users`
---
-
 INSERT INTO `users` (`id`, `name`, `role_id`) VALUES
 (1, 'Anna', 1),
 (2, 'Patrik', 1),
 (3, 'Veronica', 2);
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `vacation_requests`
---
 
 CREATE TABLE `vacation_requests` (
   `id` int NOT NULL,
@@ -79,65 +40,35 @@ CREATE TABLE `vacation_requests` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Индексы сохранённых таблиц
---
+INSERT INTO `vacation_requests` (`id`, `user_id`, `start_date`, `end_date`, `reason`, `status`, `comment`, `created_at`, `updated_at`) VALUES
+(17, 1, '2026-01-05', '2026-01-07', 'some reason', 2, 'reject', '2026-01-12 20:45:56', '2026-01-12 20:45:56');
 
---
--- Индексы таблицы `roles`
---
+
 ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`);
 
---
--- Индексы таблицы `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD KEY `users_ibfk_1` (`role_id`);
 
---
--- Индексы таблицы `vacation_requests`
---
 ALTER TABLE `vacation_requests`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- AUTO_INCREMENT для сохранённых таблиц
---
 
---
--- AUTO_INCREMENT для таблицы `roles`
---
 ALTER TABLE `roles`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT для таблицы `users`
---
 ALTER TABLE `users`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT для таблицы `vacation_requests`
---
 ALTER TABLE `vacation_requests`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
---
--- Ограничения внешнего ключа сохраненных таблиц
---
 
---
--- Ограничения внешнего ключа таблицы `users`
---
 ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
---
--- Ограничения внешнего ключа таблицы `vacation_requests`
---
 ALTER TABLE `vacation_requests`
   ADD CONSTRAINT `vacation_requests_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
